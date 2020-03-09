b2eb7955c1b4fe17a7abdd9238a66be80cee8aed
MH2033 <mh.akbari@yahoo.com>
2020-03-09 17:55:19 +0330

    Add transparent loading when pressing button

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,8 +1,10 @@
-import 'dart:ffi';
 
+import 'package:espled/pages/loading.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:custom_switch/custom_switch.dart';
+import 'package:loading/indicator/ball_pulse_indicator.dart';
+import 'package:loading/loading.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -10,8 +12,11 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  bool isLoading = false;
   IconData icon = Icons.flash_off;
   bool state = false;
+
   void changeState() {
     setState(() {});
   }
@@ -20,37 +25,65 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.teal[600],
+        backgroundColor: Colors.tealAccent[700],
         title: Text('LED Controller'),
         centerTitle: true,
       ),
       body: Center(
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.center,
+        child: Stack(
           children: <Widget>[
-            SizedBox(
-              height: 120,
-            ),
-            Icon(
-              icon,
-              size: 150,
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            Transform.scale(
-              scale: 2.5,
-              child: CupertinoSwitch(
-                activeColor: Colors.greenAccent[700],
-                value: state,
-                onChanged: (value) {
-                  setState(() {
-                    state = value;
-                    icon = state ? Icons.flash_on : Icons.flash_off;
-                  });
-                },
+            Opacity(
+              opacity: isLoading? 0.4: 1,
+              child: Container(
+                alignment: Alignment.center,
+                child: Column(
+                  //mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 120,
+                    ),
+                    Icon(
+                      icon,
+                      size: 150,
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Transform.scale(
+                      scale: 2.5,
+                      child: CupertinoSwitch(
+                        activeColor: Colors.greenAccent[700],
+                        value: state,
+                        onChanged: isLoading? null:(value) {
+                          setState(() {
+                            state = value;
+                            icon = state ? Icons.flash_on : Icons.flash_off;
+                            Future.delayed(Duration(seconds: 2)).then((onValue) {
+                              setState(() {
+                                isLoading = false;
+                              });
+
+                            });
+                            isLoading = true;
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
+            Center(
+              child: Visibility(
+                visible: isLoading,
+                child: Loading(
+                  indicator: BallPulseIndicator(),
+                  size: 100,
+                  color: Colors.tealAccent[700],
+                ),
+              ),
+            ),
+
           ],
         ),
       ),
